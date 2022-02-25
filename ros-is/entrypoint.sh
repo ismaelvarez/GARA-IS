b64123db645ebd445465f0afd0e40f777e6449f6
ismaelvarez <ismaelvarez96@gmail.com>
2022-02-25 07:44:17 +0000

    first commit

---
+++ ros-is/entrypoint.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source /opt/ros/melodic/setup.bash && roscore &
+source /opt/ros/melodic/setup.bash && source /root/is-workspace/install/setup.bash && /root/is-workspace/install/is-core/install/integration-service /root/is-workspace/config/config.yaml
